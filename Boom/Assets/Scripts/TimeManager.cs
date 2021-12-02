bf1b9b8a1b70555dcd6a0e129a021a411a87fbe3
Lars Fyhr <fyhr_lars@yahoo.com>
2021-12-02 03:30:42 -0600

    Update time manager

--- Boom/Assets/Scripts/TimeManager.cs
+++ Boom/Assets/Scripts/TimeManager.cs
@@ -8,35 +8,36 @@ public class TimeManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private List<float> levelTimes = new List<float>();
+    string totalStr;
+    bool setString = false;
 
     void Awake() 
     {
         DontDestroyOnLoad(this.gameObject);
-        //levelTimes.Add(30.64f);
-        //levelTimes.Add(100.23f);
-        //levelTimes.Add(15.41f);
-        //levelTimes.Add(70.49f);
     }
 
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Win") {
-            string totalStr = "Times:\n";
+        if (SceneManager.GetActiveScene().name == "Win" && !setString) {
+            setString = true;
             int level = 0;
             string levelStr = "";
+            totalStr = "~ Times ~\n";
             float sumTime = 0;
             foreach(float levelTime in levelTimes) {
                 if(level == 0) {
-                    levelStr = "Tutorial- ";
+                    levelStr = "Tutorial - ";
                 } else {
-                    levelStr = "Level " + level + "- ";
+                    levelStr = "Level " + level + " - ";
                 }
                 sumTime += levelTime;
                 string levelTimeStr = ((int) levelTime / 60).ToString("d2") +":"+ ((int) levelTime % 60).ToString("d2");
                 totalStr += levelStr + levelTimeStr + "\n";
                 level++;
             }
-            totalStr += "Total Time: " + ((int) sumTime / 60).ToString("d2") +":"+ ((int) sumTime % 60).ToString("d2");
+            totalStr += "Total Time - " + ((int) sumTime / 60).ToString("d2") +":"+ ((int) sumTime % 60).ToString("d2");
+        }
+        if (GameObject.Find("finaltimes") != null) {
             GameObject.Find("finaltimes").GetComponent<TMP_Text>().text = totalStr;
         }
     }
